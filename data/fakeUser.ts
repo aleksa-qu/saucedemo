29f470db4b0e056f8006f817da250e93e104d64b
37255 <aleksandra.kulakova.ndu@gmail.com>
2025-08-10 15:15:20 +0300

    made changes in faker, your information page and in test

--- data/fakeUser.ts
+++ data/fakeUser.ts
@@ -2,7 +2,13 @@ import { faker } from '@faker-js/faker';
 
 faker.seed(123);
 
-export const generateFakeUser = () => ({
+export type User = {
+  firstName: string;
+  lastName: string;
+  zipCode: string;
+};
+
+export const generateFakeUser = (): User => ({
   firstName: faker.person.firstName(),
   lastName: faker.person.lastName(),
   zipCode: faker.location.zipCode(),
--- tests/pages/your-information-page.ts
+++ tests/pages/your-information-page.ts
@@ -1,5 +1,8 @@
 import { Locator, Page } from '@playwright/test';
 import { AuthorizedPage } from './authorized-page';
+import {OverviewPage} from "./overview-page";
+import { User } from '../../data/fakeUser';
+
 
 export class YourInformationPage extends AuthorizedPage {
   readonly firstNameField: Locator;
@@ -14,8 +17,19 @@ export class YourInformationPage extends AuthorizedPage {
     this.firstNameField = page.locator('[data-test="firstName"]');
     this.lastNameField = page.locator('[data-test="lastName"]');
     this.zipPostalCodeField = page.locator('[data-test="postalCode"]');
-    this.cancelButton = page.locator('[data-test="cancel"');
+    this.cancelButton = page.locator('[data-test="cancel"]');
     this.continueButton = page.locator('[data-test="continue"]');
     this.errorMessageCheckout = page.locator('[data-test="error"]');
+  }
+
+  async fillUserInformation(user: User) {
+    await this.firstNameField.fill(user.firstName);
+    await this.lastNameField.fill(user.lastName);
+    await this.zipPostalCodeField.fill(user.zipCode);
+  }
+
+  async continueToOverview(): Promise<OverviewPage> {
+    await this.continueButton.click();
+    return new OverviewPage(this.page);
   }
 }
